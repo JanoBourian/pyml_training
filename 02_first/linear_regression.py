2878fb8591578a8b9c983fe88cb15f6c25adb6af
JanoBourian <supermath33@icloud.com>
2024-08-12 23:04:35 -0600

    add: information about linear regression

--- 02_first/linear_regression.py
+++ 02_first/linear_regression.py
@@ -2,6 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sbn
 
+def predict(x_array, w):
+    return x_array*w
+
+def loss(x_array, y_array, w):
+    return np.average((predict(x_array, w) - y_array)**2)
+
 sbn.set_theme()
 plt.axis([0, 50, 0, 50])
 plt.xticks(fontsize=8)
@@ -12,3 +18,6 @@ plt.title("Linear Regression")
 X, Y = np.loadtxt("pizza.txt", skiprows=1, unpack=True)
 plt.plot(X, Y, "bo")
 plt.show()
+
+print(f"predict: {predict(X, 1.5)}")
+print(f"loss: {loss(X, Y, 1.5)}")
