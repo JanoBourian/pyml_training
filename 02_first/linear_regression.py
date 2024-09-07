44aacce905401865a3a4320d6979a1f09e7840cb
JanoBourian <supermath33@hotmail.com>
2024-09-06 22:59:58 -0600

    add: ML model to linear regression using bias

--- 02_first/linear_regression.py
+++ 02_first/linear_regression.py
@@ -2,29 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sbn
 
-def predict(x_array, w):
-    return x_array*w
+def predict(x_array, w, b):
+    return x_array*w + b
 
-def loss(x_array, y_array, w):
-    return np.average((predict(x_array, w) - y_array)**2)
+def loss(x_array, y_array, w, b):
+    return np.average((predict(x_array, w, b) - y_array)**2)
 
 def train(x_array, y_array, iterations, lr):
     w = 0
+    b = 0
     for i in range(iterations):
-        current_loss = loss(x_array, y_array, w)
+        current_loss = loss(x_array, y_array, w, b)
         print("Iteration %4d => Loss: %.6f" % (i, current_loss))
         
         # lr: learning rate
-        if loss(x_array, y_array, w + lr) < current_loss:
+        if loss(x_array, y_array, w + lr, b) < current_loss:
             w += lr
-        elif loss(x_array, y_array, w - lr) < current_loss:
+        elif loss(x_array, y_array, w - lr, b) < current_loss:
             w -= lr
+        elif loss(x_array, y_array, w, b + lr) < current_loss:
+            b += lr
+        elif loss(x_array, y_array, w, b - lr) < current_loss:
+            b -= lr    
         else:
-            return w
+            return w, b
 
 X, Y = np.loadtxt("pizza.txt", skiprows=1, unpack=True)
-w = train(X,Y, 10000, 0.01)
-print("%.6f" % (w))
+w , b= train(X,Y, 10000, 0.01)
+print("w => %.6f, b => %.6f" % (w, b))
        
 sbn.set_theme()
 x_edge, y_edge = 50, 50
@@ -35,5 +40,5 @@ plt.xlabel("Reservations", fontsize=15)
 plt.ylabel("Pizzas", fontsize=15)
 plt.title("Linear Regression")
 plt.plot(X, Y, "bo")
-plt.plot([0, x_edge], [0, predict(x_edge, w)], linewidth=1.0, color="g")
+plt.plot([0, x_edge], [b, predict(x_edge, w, b)], linewidth=1.0, color="g")
 plt.show()
